166f3c994fbf79e3770aa731c6288517509dd219
edwanyoike <edubugz@gmail.com>
2019-02-14 14:47:11 +0300

    Update uploadFunction.java

--- uploadFunction.java
+++ uploadFunction.java
@@ -20,7 +20,11 @@ private int uploadImage(MultipartFile file) {
 
              //upload the file to the directory  ----> you can use the file original name 
              // by doing ... path.resolve(file.getOriginalName())
+                // REPLACE_EXISTING replaces a file with similar name if it exist
+                
             Files.copy(file.getInputStream(), path.resolve("admin.jpg"), REPLACE_EXISTING);
+                
+                //Files.copy(file.getInputStream(), path.resolve( file.getOriginalName() ) ); ---> this will upload all files without replacing.. exception thuough if it finds file with same name
 
             return 1;  // these returns are for chcking the upload status incase you call the method from somwhere else
             // 1 is pass, 0 is fail
